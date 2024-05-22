9a637354efa8df321681a04a0953ba31ec4b25dc
mik284 <mikeykamau222@gmail.com>
2024-05-22 14:55:55 +0300

    🎨 add: generate TImeline context and a live template

---
+++ test/support/fixtures/timeline_fixtures.ex
@@ -0,0 +1,23 @@
+defmodule TwitterClone.TimelineFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `TwitterClone.Timeline` context.
+  """
+
+  @doc """
+  Generate a post.
+  """
+  def post_fixture(attrs \\ %{}) do
+    {:ok, post} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        likes_count: 42,
+        reposts_count: 42,
+        username: "some username"
+      })
+      |> TwitterClone.Timeline.create_post()
+
+    post
+  end
+end
