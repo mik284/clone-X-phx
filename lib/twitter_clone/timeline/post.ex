0b239e926089f49fb23a42280717f8cb43a2e13e
mik284 <mikeykamau222@gmail.com>
2024-05-22 15:01:45 +0300

    add: validation to post body length

--- lib/twitter_clone/timeline/post.ex
+++ lib/twitter_clone/timeline/post.ex
@@ -14,7 +14,8 @@ defmodule TwitterClone.Timeline.Post do
   @doc false
   def changeset(post, attrs) do
     post
-    |> cast(attrs, [:username, :body, :likes_count, :reposts_count])
-    |> validate_required([:username, :body, :likes_count, :reposts_count])
+    |> cast(attrs, [:body])
+    |> validate_required([:body])
+    |> validate_length(:body, min: 2, max: 255)
   end
 end
